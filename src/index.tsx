743734bab30f554a1c7c58004dccf076c72780c9
Jason Day <jason.day.blue@gmail.com>
2022-08-21 17:16:01 -0500

    save

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+// Application Screen || Define Imports
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// import { NavBar } from "@components/navbar/navbar"
+import { NavBar } from "./components/navbar/navbar"
+import { Header } from "./components/header/header"
+import Footer from "./components/footer/footer"
 
+// Application Screen || Define Exports
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const root = ReactDOM.createRoot(document.getElementById("root")!)
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar>
+      <Header logo={null} title="TEST" link="TEST" />
+    </NavBar>
+    <Footer />
   </React.StrictMode>,
 )
-
-
