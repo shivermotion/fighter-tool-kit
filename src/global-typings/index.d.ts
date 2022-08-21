b3f61a4d2de6fd7d8c195dedf8d2d7a8a25f1d59
Jason Day <jason.day.blue@gmail.com>
2022-08-21 12:31:58 -0500

    blank slate

---
+++ src/global-typings/index.d.ts
@@ -0,0 +1,12 @@
+// Application Assets || File Type Declarations
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Typescript relies on this file resolve unfamiliar file types  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+declare module "*.css";
+declare module "*.sass";
+declare module "*.scss";
+declare module "*.json";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
