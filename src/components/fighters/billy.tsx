d2c24dee322a283232cdc610398827b249175dee
Jason Day <jason.day.blue@gmail.com>
2022-09-06 20:55:01 -0500

    completed

---
+++ src/components/fighters/billy.tsx
@@ -0,0 +1,89 @@
+import React from "react"
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Grid, Divider } from "@mui/material"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+
+// Application Screen || Define Imports
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+import { CloseBy, InAir, PressInOrder, AfterMove, InTimeWith, Charge, LinkTo, And, AirOk } from "../icons/condition"
+import { CommandMoves, SpecialMoves, SuperSpecialMoves, ClimaxSuperSpecialMoves, NormalThrows, MoveName, InputBar } from "../accordion/accordion"
+import Press from "../icons/press"
+import Or from "../icons/or"
+import Plus from "../icons/plus"
+import right from "../../assets/icons/right.png"
+import left from "../../assets/icons/left.png"
+import up from "../../assets/icons/up.png"
+import down from "../../assets/icons/down.png"
+import downRight from "../../assets/icons/downRight.png"
+import halfCircleFoward from "../../assets/icons/halfCircleForward.png"
+import halfCircleBack from "../../assets/icons/halfCircleBack.png"
+import shory from "../../assets/icons/shory.png"
+import reverseHadoken from "../../assets/icons/reverseHadoken.png"
+import hadoken from "../../assets/icons/hadoken.png"
+import punchHigh from "../../assets/icons/punchHigh.png"
+import punchLow from "../../assets/icons/punchLow.png"
+import kickHigh from "../../assets/icons/kickHigh.png"
+import kickLow from "../../assets/icons/kickLow.png"
+import upRight from "../../assets/icons/upRight.png"
+import upLeft from "../../assets/icons/upLeft.png"
+
+
+// Application Screen || Define Exports
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+export default function Billy() {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+        sx={{ backgroundColor: "black", color: "whitesmoke", fontFamily: "montserrat" }}
+      >
+        <Typography>BILLY KANE</Typography>
+      </AccordionSummary>
+      <CommandMoves />
+      <MoveName move="DAI KAITEN-GERI" />
+      <InputBar><Press icon={right} /><Plus /><Press icon={punchLow} /></InputBar>
+      <MoveName move=" BOUTAKATOBI-GERI" />
+      <InputBar><Press icon={right} /><Plus /><Press icon={kickLow} /></InputBar>
+      <MoveName move="CHOKKA UCHI" />
+      <InputBar><InAir /><Press icon={down} /><Plus /><Press icon={punchHigh} /></InputBar>
+      <SpecialMoves />
+      <MoveName move="SENPUU KON [EX]" />
+      <InputBar><Press icon={shory} /><Plus /><Press icon={punchLow} /><Or /><Press icon={punchHigh} /></InputBar>
+      <MoveName move="SANSETSU KON CHUUDAN UCHI [EX]" />
+      <InputBar><Press icon={halfCircleFoward} /><Plus /><Press icon={punchLow} /><Or /><Press icon={punchHigh} /></InputBar>
+      <MoveName move=" KAEN SANSETSU KON CHUUDAN UCHI [EX]" />
+      <InputBar><AfterMove move="[" /><Press icon={punchHigh} /> <Or /> <AfterMove move="[EX] SANSETSU KON CHUUDAN UCHI]" /><Press icon={hadoken} /><Plus /><Press icon={punchLow} /></InputBar>
+      <MoveName move=" TSURIGOME SANSETSU KON CHUUDAN UCHI" />
+      <InputBar><AfterMove move="[" /><Press icon={punchHigh} />  <AfterMove move=" SANSETSU KON CHUUDAN UCHI]" /><Press icon={hadoken} /><Plus /><Press icon={punchHigh} /></InputBar>
+      <MoveName move=" KYOUSHUU HISHOU KON [EX]" />
+      <InputBar><Press icon={shory} /><Plus /><Press icon={kickLow} /><Or /><Press icon={kickHigh} /></InputBar>
+      <MoveName move="SUZUME OTOSHI [EX]" />
+      <InputBar><Press icon={reverseHadoken} /><Plus /><Press icon={punchLow} /><Or /><Press icon={punchHigh} /></InputBar>
+      <MoveName move=" KAEN SUZUME OTOSHI [EX]" />
+      <InputBar><AfterMove move="[" /><Press icon={punchHigh} /> <Or /> <AfterMove move="[EX] SUZUME OTOSHI]" /><Press icon={hadoken} /><Plus /><Press icon={punchLow} /><Or /><Press icon={punchHigh} /></InputBar>
+      <MoveName move="KA RYUU TSUIGEKI KON [EX]" />
+      <InputBar><Press icon={reverseHadoken} /><Plus /><Press icon={kickLow} /></InputBar>
+      <MoveName move=" SUI RYUU TSUIGEKI KON" />
+      <InputBar><Press icon={reverseHadoken} /><Plus /><Press icon={kickHigh} /></InputBar>
+      <SuperSpecialMoves />
+      <MoveName move=" CHOU KAEN SENPUU KON [MAX]" />
+      <InputBar><Press icon={hadoken} /><Press icon={halfCircleBack} /><Plus /><Press icon={punchLow} /><Or /><Press icon={punchHigh} /></InputBar>
+      <MoveName move=" GUREN SAKKON" />
+      <InputBar><Press icon={hadoken} /><Press icon={hadoken} /><Press icon={kickLow} /> <Or /><Press icon={kickHigh} /></InputBar>
+      <ClimaxSuperSpecialMoves />
+      <MoveName move="SYAKU NETU KAISEN KON" />
+      <InputBar><Press icon={reverseHadoken} /><Plus /><Press icon={punchHigh} /><Press icon={kickHigh} /></InputBar>
+      <NormalThrows />
+      <MoveName move="JIGOKU OTOSHI" />
+      <InputBar><CloseBy /><Press icon={left} /><Or /><Press icon={right} /><Plus /><Press icon={punchHigh} /></InputBar>
+      <MoveName move="IPPON TSURI NAGE" />
+      <InputBar><CloseBy /><Press icon={left} /><Or /><Press icon={right} /><Plus /><Press icon={kickHigh} /></InputBar>
+    </Accordion>
+  )
+}
+
+
